c9141bfbe93cb038d662a951157a5c020a62040e
Mucahid <m.eker37@hotmail.com>
2021-12-23 14:46:24 +0300

    Send Multiple Mail From List With Python

--- maill.py
+++ maill.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase 
+#from email.mime.base import MIMEBase 
 
 #The mail addresses and  password
 sender_address = 'xxxx@xxxxxx.com' 
